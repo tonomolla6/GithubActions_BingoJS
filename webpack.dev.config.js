f112cbdd28ab72adf72df950c111622576556177
tonomolla6 <tonomollag6@gmail.com>
2021-01-31 13:58:53 +0100

    Tono -> Job de generación de estáticos

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,22 +1,25 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000', './src/frontend/main.js']
+    main: [
+      "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000",
+      "./src/frontend/main.js",
+    ],
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.join(__dirname, "build"),
+    publicPath: "/",
+    filename: "[name].js",
   },
   externals: {
-    socketio: 'socket.io-client',    
+    socketio: "socket.io-client",
   },
-  mode: 'development',
-  target: 'web',
-  devtool: '#source-map',
+  mode: "development",
+  target: "web",
+  devtool: "#source-map",
   module: {
     rules: [
       {
@@ -27,8 +30,8 @@ module.exports = {
         options: {
           emitWarning: true,
           failOnError: false,
-          failOnWarning: false
-        }
+          failOnWarning: false,
+        },
       },
       {
         test: /\.js$/,
@@ -37,42 +40,40 @@ module.exports = {
       },
       {
         // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
+        // Entry point is set below in HtmlWebPackPlugin in Plugins
         test: /\.html$/,
         use: [
           {
             loader: "html-loader",
             //options: { minimize: true }
-          }
-        ]
-      },
-      { 
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+          },
+        ],
       },
       {
-       test: /\.(png|svg|jpg|gif)$/,
-       use: ['file-loader']
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
       },
       {
         test: /\.mp4$/,
-        use: ['file-loader?name=videos/[name].[ext]']
-       },
-       {
-
+        use: ["file-loader?name=videos/[name].[ext]"],
+      },
+      {
         test: /\.mp3$/,
-        use: ['file-loader?name=audio/[name].[ext]']
-
-      }
-    ]
+        use: ["file-loader?name=audio/[name].[ext]"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/frontend/index.html",
       filename: "./index.html",
-      excludeChunks: [ 'server' ]
+      excludeChunks: ["server"],
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
-}
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+};
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,23 +1,23 @@
-const path = require('path')
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: './src/frontend/main.js'
+    main: "./src/frontend/main.js",
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.join(__dirname, "build"),
+    publicPath: "/",
+    filename: "[name].js",
   },
   externals: {
-    socketio: 'socket.io-client',    
+    socketio: "socket.io-client",
   },
-  target: 'web',
-  devtool: '#source-map',
+  target: "web",
+  devtool: "#source-map",
   // Webpack 4 does not have a CSS minifier, although
   // Webpack 5 will likely come with one
   optimization: {
@@ -25,10 +25,10 @@ module.exports = {
       new UglifyJsPlugin({
         cache: true,
         parallel: true,
-        sourceMap: true // set to true if you want JS source maps
+        sourceMap: true, // set to true if you want JS source maps
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
   module: {
     rules: [
@@ -37,55 +37,53 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
+        // Entry point is set below in HtmlWebPackPlugin in Plugins
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
       },
       {
         // Loads images into CSS and Javascript files
         test: /\.jpg$/,
-        use: [{loader: 'url-loader'}]
+        use: [{ loader: "url-loader" }],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-       },
-       {
-         test: /\.mp4$/,
-         use: ['file-loader?name=videos/[name].[ext]']
-        },
-        {
- 
-         test: /\.mp3$/,
-         use: ['file-loader?name=audio/[name].[ext]']
- 
-       },
+        use: ["file-loader"],
+      },
+      {
+        test: /\.mp4$/,
+        use: ["file-loader?name=videos/[name].[ext]"],
+      },
+      {
+        test: /\.mp3$/,
+        use: ["file-loader?name=audio/[name].[ext]"],
+      },
       {
         // Loads CSS into a file when you import it via Javascript
         // Rules are set in MiniCssExtractPlugin
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/frontend/index.html',
-      filename: './index.html'
+      template: "./src/frontend/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ]
-}
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
