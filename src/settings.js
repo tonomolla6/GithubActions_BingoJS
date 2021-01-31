825e24d84a344ad07dfcb1940fb0e315f0c7624c
tonomolla6 <tonomollag6@gmail.com>
2021-01-31 21:42:49 +0100

    Tono -> Practica finalizada

--- src/settings.js
+++
@@ -1,8 +0,0 @@
-const settings = {
-  ballspeed: 1, //Online and offline applied
-  secsUntilBegin: 15, //Online applied
-  maxUsers: 10, //Online applied
-  secsLineaWait: 5, //Online applied
-};
-
-export { settings };
